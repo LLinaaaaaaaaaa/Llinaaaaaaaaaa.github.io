092db8bb02e9cc5f0d30be2f95f12c8c0cd6ba3b
m <140096350+showmercyy@users.noreply.github.com>
2024-04-25 22:49:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-document.getElementById("continuebutton").addEventListener("click", function() {
-    document.getElementById("continuebutton").style.display = "none";
-    document.getElementById("restofsite").style.display = "block";
-    const site = document.getElementById("site");
-    site.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1135592344847142952/1136152147705200750/IMG_0420.jpg')";
-    const files = [
-        "./songs/slut.mp3",
-        "./songs/ho.mp3",
-        "./songs/homie.mp3",
-        "./songs/bmpu.mp3",
-        "./songs/ddlg.mp3",
-    ];
-    const index = Math.floor(Math.random() * files.length);
-    const audioplayer = document.getElementById("musicplayer");
-    audioplayer.src = files[index];
-    audioplayer.play();
-    audioplayer.loop = true;
-});
+const typedTextSpan = document.querySelector(".typed");
+const cursorSpan = document.querySelector(".cursor");
 
+const textArray = ["tryna make it"];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 1000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+            typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(type, typingDelay);
+        } else {
+            cursorSpan.classList.remove("typing");
+                setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+            typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+            charIndex--;
+            setTimeout(erase, erasingDelay);
+        } else {
+            cursorSpan.classList.remove("typing");
+            textArrayIndex++;
+        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+              setTimeout(type, typingDelay + 1100);
+        }
+    }
